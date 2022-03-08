0988ae56464dc9fcd4bf0eadd562a5d351a8148c
Emerson <emersontec147@gmail.com>
2022-03-08 18:22:32 -0500

    Tercer Comit

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { HiMenuAlt4, HiX} from 'react-icons/hi';
+import {motion} from 'framer-motion';
+
 
 import { images } from "../../constants";
 import './Navbar.scss'
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'> 
         <img src={images.logo} alt="logo" />
       </div>
-      <ul>
-        {['home', 'about', 'contact', 'work', 'skills', 'contact'].map((item) => )}
-      </ul>
+     <ul className='app__navbar-links'>
+       {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+         <li className='app__flex p-text' key={`link-${item}`}>
+           <div />
+             <a href={`#${item}`}>{item}</a>
+         </li>
+       ))}
+     </ul>
+
+      <div className='app__navbar-menu'>
+        <HiMenuAlt4 onClick={() => setToggle(true)}
+      </div>
+
 
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar      
